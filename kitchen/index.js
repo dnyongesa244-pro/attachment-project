b351c27c56949ed7f55cc1305d81302570d68e51
DANIEL KOKONYA NYONGESA <dyongesa244@gmail.com>
2024-07-18 00:27:29 +0300

    Added user authonitification

--- kitchen/index.js
+++ kitchen/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -6,179 +7,157 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const hbs = require('hbs');
 const multer = require('multer');
-//const { type } = require('os');
 const storage = multer.memoryStorage();
-const upload = multer();
+const upload = multer({ storage });
+const mongoStore = require('connect-mongo')
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 app.engine('hbs', exphbs.engine({
-    extname : ".hbs",
-    partialsDir : path.join(__dirname,"views",'partials'),
-    layoutDir : path.join(__dirname,'views','layouts')
+    extname: ".hbs",
+    partialsDir: path.join(__dirname, "views", 'partials'),
+    layoutsDir: path.join(__dirname, 'views', 'layouts')
 }));
 
-app.set('view engine','hbs');
-app.set('views',path.join(__dirname,'views'));;
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret : "shh its a secreat",
+    resave : false,
+    saveUninitialized : false,
+    store : mongoStore.create({
+        mongoUrl : "mongodb://localhost:27017/kitchen",
+        collectionName : "sessions"
+    }),
+    cookie : {maxAge : 24*60*60*1000}
+}))
 
-//setting upp mongodb database connection
-mongoose.connect('mongodb://localhost:27017/kitchen')
-.then(()=>{
-    console.log('database connected succesfully');
-    app.listen(port,()=>{
-        console.log(`server rinning on port ${port}`);
-    });
-})
-.catch(err=>{
-    console.log(err);
-});
-
-//defining the database schema
-const personSchema = mongoose.Schema({
-    fname : {
-        type : String,
-        required : true
-    },
-    sname : {
-        type : String
-    },
-    lname : {
-        type : String,
-        required : true
-    },
-    username : {
-        type : String,
-        required : true
-    },
-    dept : {
-        required : true,
-        type : String
-    },
-    email : {
-        required : true,
-        type :String
-    },
-    phoneNo : {
-        type : Number,
-        required : true
-    },
-    role : {
-        type : String,
-        required : true
-    },
-    password : {
-        type : String,
-        required : true
-    }
-});
-
-//definng the collection
-const staff = mongoose.model('staff',personSchema);
-
-//getting the addnewuser page
-app.get('/addNew',(req, res)=>{
-    res.render('addNew');
-})
-
-//posting the new user to the database
-app.post('/addnew',async(req, res)=>{
-    //getting the response from the addnew page form
-    const personInfo = req.body;
-    //cheking if all the required data have been recieved
-    if(!personInfo.lname||!personInfo.lname||!personInfo.username||!personInfo.dept||!personInfo.email||!personInfo.password||!personInfo.phoneNo||!personInfo.role){
-        res.send('please enter correct information');//sending response for incorect infotmation
-        //res.render('addUser');
+function isAutaumticated(req, res, next){
+    if(req.session.userId){
+        return next();
     } else{
-        const check = await staff.findOne({username : personInfo.username},{email : personInfo.email});
-        if(check.username===personInfo.username && check.email === personInfo.password){
-            res.send(`user with username : ${check.username} and email: ${check.email} already exist`);
-        } else if(check.username === personInfo.username){
-            res.send(`username : ${check.username} already exist please try a differnd username`);
-        } else if(check.email===personInfo.email){
-            res.send(`user with email : ${check.email} already exist`);
-        } else {
-            const  newPerson = new staff({
-                fname : personInfo.fname,
-                sname : personInfo.sname,
-                lname : personInfo.lname,
-                username : personInfo.username,
-                dept : personInfo.dept,
-                email : personInfo.email,
-                phoneNo : personInfo.phoneNo,
-                role : personInfo.role,
-                password : personInfo.password
-            });
+        res.redirect('/login');
+    }
+}
+// Setting up MongoDB database connection
+mongoose.connect('mongodb://localhost:27017/kitchen')
+    .then(() => {
+        console.log('Database connected successfully');
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
-            try {
+// Defining the database schema
+const personSchema = new mongoose.Schema({
+    fname: { type: String, required: true },
+    sname: { type: String },
+    lname: { type: String, required: true },
+    username: { type: String, required: true },
+    dept: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNo: { type: Number, required: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+// Defining the collection
+const staff = mongoose.model('staff', personSchema);
+
+// Getting the add new user page
+app.get('/addNew',isAutaumticated, (req, res) => {
+    res.render('addNew');
+});
+
+// Posting the new user to the database
+app.post('/addnew', isAutaumticated, async (req, res) => {
+    const personInfo = req.body;
+
+    if (!personInfo.fname || !personInfo.lname || !personInfo.username || !personInfo.dept || !personInfo.email || !personInfo.password || !personInfo.phoneNo || !personInfo.role) {
+        res.send('Please enter correct information');
+    } else {
+        try {
+            const check = await staff.findOne({ $or: [{ username: personInfo.username }, { email: personInfo.email }] });
+            if (check) {
+                if (check.username === personInfo.username && check.email === personInfo.email) {
+                    res.send(`User with username: ${check.username} and email: ${check.email} already exist`);
+                } else if (check.username === personInfo.username) {
+                    res.send(`Username: ${check.username} already exists, please try a different username`);
+                } else if (check.email === personInfo.email) {
+                    res.send(`User with email: ${check.email} already exists`);
+                }
+            } else {
+                const newPerson = new staff({
+                    fname: personInfo.fname,
+                    sname: personInfo.sname,
+                    lname: personInfo.lname,
+                    username: personInfo.username,
+                    dept: personInfo.dept,
+                    email: personInfo.email,
+                    phoneNo: personInfo.phoneNo,
+                    role: personInfo.role,
+                    password: personInfo.password
+                });
+
                 await newPerson.save();
-                res.send('New user created succesfully');
-            } catch(err){
-                console.log(err);
-                res.send('an error occures  ');
-                res.send(err);
-            }try {
-                await newPerson.save();
-                res.send('New user created succesfully');
-            } catch(err){
-                console.log(err);
-                res.send('an error occures  ');
-                res.send(err);
+                res.send('New user created successfully');
             }
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('An error occurred');
         }
     }
 });
-//displaying login form
-app.get('/login',(req, res)=>{
+
+// Displaying login form
+app.get('/login', (req, res) => {
     res.render('login');
 });
 
-
-//verifying login and open of home page
-app.post('/login',async(req, res)=>{
+// Verifying login and opening of home page
+app.post('/login', async (req, res) => {
     const personInfo = req.body;
-    if(!personInfo.username||!personInfo.password){
-        res.send('please enter the valid details');
-    } else{
-        try{
-            const check = await staff.findOne({password : personInfo.password,username : personInfo.username});
-            if(check && check.password=== personInfo.password && check.username === personInfo.username){
+
+    if (!personInfo.username || !personInfo.password) {
+        res.send('Please enter the valid details');
+    } else {
+        try {
+            const check = await staff.findOne({ username: personInfo.username, password: personInfo.password });
+            if (check) {
+                req.session.userId = check._id;
                 res.redirect('/home');
-            } else{
-                res.send('incorect username or password');
+            } else {
+                res.send('Incorrect username or password');
             }
-        } catch(err){
+        } catch (err) {
             console.log(err);
-            res.status(500).send('internal server error');
+            res.status(500).send('Internal server error');
         }
     }
 });
 
-//defingin schema for order items
+// Defining schema for order items
 const orderItemsSchema = new mongoose.Schema({
-    name: {
-        type : String,
-        required : true
-    },
-    price: {
-        type : Number,
-        required : true
-    },
-    image: {
-        data: Buffer,
-        contentType: String
-    }
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { data: Buffer, contentType: String }
 });
 
-const orderItems = mongoose.model('orderItems',orderItemsSchema);
+const orderItems = mongoose.model('orderItems', orderItemsSchema);
 
-//defingin collections for order items
-app.get('/additem',(req, res)=>{
+// Defining collections for order items
+app.get('/additem',isAutaumticated, (req, res) => {
     res.render('add');
 });
 
-//adddind of food item
-app.post('/upload', upload.single('image'), async (req, res) => {
+// Adding of food item
+app.post('/upload',isAutaumticated, upload.single('image'), async (req, res) => {
     const orderItemsInfo = req.body;
     const image = req.file;
 
@@ -204,26 +183,26 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     }
 });
 
-
-app.get('/home', async(req, res)=>{
+// Rendering home page with products
+app.get('/home', isAutaumticated, async (req, res) => {
     try {
         const products = await orderItems.find({});
-        const productsWithBase64Images = products.map(orderItem=>{
-            if(orderItem.image && orderItem.image.data && orderItem.image.contentType){
+        const productsWithBase64Images = products.map(orderItem => {
+            if (orderItem.image && orderItem.image.data && orderItem.image.contentType) {
                 return {
                     ...orderItem._doc,
-                    image : {
-                        data : orderItem.image.data.toString('base64'),
-                        contentType : orderItem.image.constentType
+                    image: {
+                        data: orderItem.image.data.toString('base64'),
+                        contentType: orderItem.image.contentType
                     }
-                }
+                };
             } else {
-                return orderItem._doc
+                return orderItem._doc;
             }
-        })
-        res.render('home', {products : productsWithBase64Images});
-    } catch(err){
-        console.log('an error occured', err);
-        res.status(500).send('Error occured');
+        });
+        res.render('home', { products: productsWithBase64Images });
+    } catch (err) {
+        console.log('An error occurred', err);
+        res.status(500).send('Error occurred');
     }
 });
